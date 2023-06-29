bd2aebc362d5aed3d7b59c4abd7c9e4ba3b6cca9
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2023-06-29 11:27:19 +0200

    Create grant.ps1
    Signed-off-by: Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>

---
+++ grant.ps1
@@ -0,0 +1,112 @@
+######################################################################
+# HelloID-Conn-Prov-Target-OutSystems-RoleManagement-Entitlement-Grant
+#
+# Version: 1.0.0
+######################################################################
+# Initialize default values
+$config = $configuration | ConvertFrom-Json
+$p = $person | ConvertFrom-Json
+$aRef = $AccountReference | ConvertFrom-Json
+$pRef = $permissionReference | ConvertFrom-Json
+$success = $false
+$auditLogs = [System.Collections.Generic.List[PSCustomObject]]::new()
+
+# Enable TLS1.2
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+# Set debug logging
+switch ($($config.IsDebug)) {
+    $true { $VerbosePreference = 'Continue' }
+    $false { $VerbosePreference = 'SilentlyContinue' }
+}
+
+#region functions
+function Resolve-OutSystemsRoleManagementError {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [object]
+        $ErrorObject
+    )
+    process {
+        $httpErrorObj = [PSCustomObject]@{
+            ScriptLineNumber = $ErrorObject.InvocationInfo.ScriptLineNumber
+            Line             = $ErrorObject.InvocationInfo.Line
+            ErrorDetails     = $ErrorObject.Exception.Message
+            FriendlyMessage  = $ErrorObject.Exception.Message
+        }
+
+        try {
+            $errorDetails = $ErrorObject.ErrorDetails | ConvertFrom-Json
+            $httpErrorObj.ErrorDetails = "Exception: $($ErrorObject.Exception.Message), Error: $($errorDetails.Errors), code: $($errorDetails.StatusCode)"
+            $httpErrorObj.FriendlyMessage = "Error: $($errorDetails.Errors), code: $($errorDetails.StatusCode)"
+        } catch {
+            $httpErrorObj.FriendlyMessage = "Received an unexpected response. The JSON could not be converted, error: [$($_.Exception.Message)]. Original error from web service: [$($ErrorObject.Exception.Message)]"
+        }
+        Write-Output $httpErrorObj
+    }
+}
+#endregion
+
+# Begin
+try {
+    # Verify if [aRef] has a value
+    if ([string]::IsNullOrEmpty($($aRef))) {
+        throw 'The account reference could not be found'
+    }
+
+    # Add an auditMessage showing what will happen during enforcement
+    if ($dryRun -eq $true) {
+        Write-Warning "[DryRun] Grant OutSystems entitlement: [$($pRef.DisplayName)] to: [$($p.DisplayName)] will be executed during enforcement"
+    }
+
+    # Process
+    if (-not($dryRun -eq $true)) {
+        Write-Verbose "Granting OutSystems entitlement: [$($pRef.DisplayName)]"
+        $headers = [System.Collections.Generic.Dictionary[string, string]]::new()
+        $headers.Add("Authorization", "Basic $([System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$($config.UserName):$($config.Password)")))")
+
+        $baseUrl = ($($config.BaseUrl)).Replace("{environment}", $pRef.Environment)
+        $splatParams = @{
+            Uri = "$baseUrl/$($config.API)/rest/usermanagement/portalusers"
+            Method = 'POST'
+            Body = @{
+                UserName = $aRef
+                GroupName = $pRef.Reference
+            } | ConvertTo-Json
+            Headers = $headers
+            ContentType = 'application/json'
+            Verbose = $false
+        }
+        $null = Invoke-RestMethod @splatParams
+
+        $success = $true
+        $auditLogs.Add([PSCustomObject]@{
+                Message = "Grant OutSystems entitlement: [$($pRef.DisplayName)] was successful"
+                IsError = $false
+            })
+    }
+} catch {
+    $success = $false
+    $ex = $PSItem
+    if ($($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or
+        $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
+        $errorObj = Resolve-OutSystemsRoleManagementError -ErrorObject $ex
+        $auditMessage = "Could not grant OutSystems entitlement: [$($pref.DisplayName)]. Error: $($errorObj.FriendlyMessage)"
+        Write-Verbose "Error at Line '$($errorObj.ScriptLineNumber)': $($errorObj.Line). Error: $($errorObj.ErrorDetails)"
+    } else {
+        $auditMessage = "Could not grant OutSystems entitlement: [$($pref.DisplayName)]. Error: $($ex.Exception.Message)"
+        Write-Verbose "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($ex.Exception.Message)"
+    }
+    $auditLogs.Add([PSCustomObject]@{
+            Message = $auditMessage
+            IsError = $true
+        })
+# End
+} finally {
+    $result = [PSCustomObject]@{
+        Success   = $success
+        Auditlogs = $auditLogs
+    }
+    Write-Output $result | ConvertTo-Json -Depth 10
+}
